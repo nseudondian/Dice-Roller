64bf135bc35630562cf24b9e97f89e80ebbf6ea7
Nseabasi Udondian <nseabasiudondian@gmail.com>
2023-03-26 16:55:17 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import random
+
+
+def dice_roller():
+
+    roll = input("Wanna roll the dice? (Yes/No): ")
+
+    while roll.lower() == "Yes".lower():
+        dice1 = random.randint(1,6)
+        dice2 = random.randint(1, 6)
+
+        print("dice rolled: {} and {}".format(dice1, dice2))
+
+        roll = input("roll again? (Yes/No): ")
+    
+dice_roller()
